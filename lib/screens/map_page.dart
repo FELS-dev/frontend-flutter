2e626d44f2f90fe0d685cc2b7769ce93c1fd4888
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-05-23 14:42:16 +0200

    feat: permission for location

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,11 +9,59 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  bool _hasPermissions = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _fetchPermissionStatus();
+  }
+  
+  void _fetchPermissionStatus() {
+    Permission.locationWhenInUse.status.then((value) {
+      if (mounted) {
+        setState(() {
+          _hasPermissions = (value == PermissionStatus.granted);
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Page de navigation'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Page de navigation'),
+        ),
+        body: Builder(builder: (context) {
+          if (_hasPermissions) {
+            return _buildCompass();
+          } else {
+            return _buildPermissionSheet();
+          }
+        },),
+      ),
+    );
+  }
+
+  Widget _buildCompass() {
+    return Container(
+      child: const Text('Compass here')
+    );
+  }
+
+  Widget _buildPermissionSheet() {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Request permissions'),
+        onPressed: () {
+          Permission.locationWhenInUse.request().then((value) {
+            _fetchPermissionStatus();
+          });
+        },
       ),
     );
   }
