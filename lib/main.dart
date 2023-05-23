287241f58f9f68a90e72bcc33ca4f829f817b970
Fabien Fiette <fabienfiette@hotmail.fr>
2023-05-23 21:33:41 +0200

    refacto: Revert flutter_map package to a custom map with InteractiveViewer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,29 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: FlutterMap(
-          options: MapOptions(
-            center: LatLng(48.8586476, 2.3740813),
-            zoom: 17,
-            maxZoom: 18,
-            minZoom: 0,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example.app',
-            ),
-            MarkerLayer(
-              markers: [
-                Marker(
-                  point: LatLng(48.8586476, 2.3740813),
-                  width: 80,
-                  height: 80,
-                  builder: (context) => const FlutterLogo(),
-                ),
-              ],
-            ),
-          ],
+        body: InteractiveViewer(
+          minScale: 0.1,
+          maxScale: 10.0,
+          scaleEnabled: true,
+          child: Image.asset('assets/eventMap.png'),
         ),
       ),
     );
