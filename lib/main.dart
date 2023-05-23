4c5b7a7c2d3c63c818a7f451802a507a941d0c77
Fabien Fiette <fabienfiette@hotmail.fr>
2023-05-23 21:59:27 +0200

    feat: list all pins and center interactive view

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Temp pins map data
+    final List<Map<String, double>> markers = [
+      {'x': 50.0, 'y': 360.0},
+      {'x': 300.0, 'y': 420.0},
+    ];
+
     return MaterialApp(
       home: Scaffold(
-        body: InteractiveViewer(
-          minScale: 0.1,
-          maxScale: 10.0,
-          scaleEnabled: true,
-          child: Stack(
-            children: <Widget>[
-              Image.asset('assets/eventMap.png'),
-              Positioned(
-                left: 100,
-                top: 100,
-                child: Container(
-                  width: 30,
-                  height: 30,
-                  decoration: const BoxDecoration(
-                    color: Colors.pinkAccent,
-                    shape: BoxShape.circle,
+        body: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: InteractiveViewer(
+            minScale: 0.1,
+            maxScale: 10.0,
+            scaleEnabled: true,
+            child: Stack(
+              children: <Widget>[
+                Image.asset(
+                  'assets/eventMap.png',
+                  height: double.infinity,
+                  width: double.infinity,
+                ),
+                // List all pins
+                ...markers.map(
+                  (marker) => Positioned(
+                    left: marker['x'],
+                    top: marker['y'],
+                    child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                        color: Colors.pinkAccent,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
