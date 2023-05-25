e046691447d6ecbdcd92d13e215da01da486f980
Lucas Malatchoumy <malatchoumylucas.com>
2023-05-25 15:13:27 +0200

    feat(QRCodePage): Get QR Code from cache and show it

--- lib/screens/qr_code.dart
+++ lib/screens/qr_code.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QRCodePage extends StatefulWidget {
   const QRCodePage({super.key});
@@ -9,10 +12,92 @@ class QRCodePage extends StatefulWidget {
 
 class _QRCodePageState extends State<QRCodePage> {
   // IMPORTER LE QR CODE
+  Future<String?> _getQRCode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? action = prefs.getString('cachedQRCode');
+    return action;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('IMPORTER LE QR CODE'),
+    return Stack(
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FutureBuilder<String?>(
+                  future: _getQRCode(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return QrImageView(
+                        data: snapshot.data!,
+                        version: QrVersions.auto,
+                        size: 200.0,
+                      );
+                    } else {
+                      // Gérer le cas où les données ne sont pas encore disponibles
+                      return CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
+
+
+// class QRCodePage extends StatefulWidget {
+//   const QRCodePage({super.key});
+
+//   @override
+//   State<QRCodePage> createState() => _QRCodePageState();
+// }
+
+// class _QRCodePageState extends State<QRCodePage> {
+//   // IMPORTER LE QR CODE
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Positioned(
+//       top: 0,
+//       left: 0,
+//       right: 0,
+//       bottom: 0,
+//       child: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             FutureBuilder<String?>(
+//               future: _getQRCode(),
+//               builder: (context, snapshot) {
+//                 if (snapshot.hasData) {
+//                   return QrImageView(
+//                     data: snapshot.data!,
+//                     version: QrVersions.auto,
+//                     size: 200.0,
+//                   );
+//                 } else {
+//                   // Gérer le cas où les données ne sont pas encore disponibles
+//                   return CircularProgressIndicator();
+//                 }
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
